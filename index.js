457c33574d59795e5c4ba41e06d00a2f5683eb74
Sven Piller <sven.piller@gmail.com>
2016-07-30 17:35:21 +0200

    added console output

--- index.js
+++ index.js
@@ -1,53 +1,8 @@
+'use strict';
+
 /**
- * Escape special characters in the given string of html.
+ * Export lib/mongoose
  *
- * @param  {String} message
- * @param  {String} level
- * @param  {String} indicator
- * @param  {String} destiny
- * @return {String}
  */
-module.exports = {
-  log: function(message, level, indicator, destiny) {
-    console.log(level + '  ' + indicator + '  ' + message);
-  },
 
-  /**
-   * Escape special characters in the given string of html.
-   *
-   * @return {String}
-   */
-  log2: function() {
-    console.log('log2');
-  },
-
-  /**
-   * Escape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
-   */
-  escape: function(html) {
-    return String(html)
-      .replace(/&/g, '&amp;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;');
-  },
-
-  /**
-   * Unescape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
-   */
-  unescape: function(html) {
-    return String(html)
-      .replace(/&amp;/g, '&')
-      .replace(/&quot;/g, '"')
-      .replace(/&#39;/g, '\'')
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>');
-  }
-}
+module.exports = require('./lib/');
